f6d9222dbd6e211ceeda708b4670b1f78bb3d3b8
majamarijan <majapianist@gmail.com>
2026-02-07 21:04:07 +0100

    create /server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, Suspense } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState<string>('');
+  useEffect(()=> {
+    setTimeout(() => {
+      fetch('http://localhost:4000/api/hello', {credentials: 'include'}).then(res=> res.json()).then(res=> setMessage(res.message));
+    }, 1000);
+  },[]);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello from React app!</h1>
+        <Suspense fallback={<p>Loading...</p>}>
+        <p>Message from server: {message ? message : 'Loading...'}</p>
+        </Suspense>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -9,22 +9,17 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 dotenv.config();
 const app = express();
-//console.log(process.env.TOKEN_SECRET_KEY);
+
 app.use(favicon(path.join(import.meta.dirname,'public', 'logo.ico')));
-app.use(express.static(path.join(import.meta.dirname,'public')));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-app.use((req,res,next)=> {
-	console.log('Running');
-	next();
-});
-app.use('/', mainRouter);
-app.use('/secure', loginRouter);
-app.use('/users', userRouter);
-app.post('/data',(req,res)=> {
-	res.send("You sent ",JSON.stringify(req.body));
-});
+app.use(cors({
+	origin: 'http://localhost:5173',
+	credentials: true
+}));
+app.use('/api/hello', (req,res)=> {
+	res.json({message: 'Hello from express app.'})
+})
 app.use((err,req,res,next)=>{
 	res.status(500).send("Server error!");
 });

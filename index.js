2c7a30d9d9e200cecfba19f818c822780d544526
majamarijan <majapianist@gmail.com>
2026-02-07 19:27:29 +0100

    add react-ts

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import path from 'path';
 import favicon from 'serve-favicon';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 dotenv.config();
 const app = express();
 //console.log(process.env.TOKEN_SECRET_KEY);
@@ -13,6 +14,7 @@ app.use(favicon(path.join(import.meta.dirname,'public', 'logo.ico')));
 app.use(express.static(path.join(import.meta.dirname,'public')));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use((req,res,next)=> {
 	console.log('Running');
 	next();
